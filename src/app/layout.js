5e9ffb04301803caa61132634aec4925698eacb1
kishorebarathR <kishorebarath011099@gmail.com>
2026-01-19 16:39:41 +0530

    added pixel in layout page

--- src/app/layout.js
+++ src/app/layout.js
@@ -163,6 +163,37 @@ const RootLayout = ({ children }) => {
         </Script>
         {/* End Google tag (gtag.js) */}
 
+        {/* Facebook Pixel - Only load on production domain */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+          if (window.location.hostname === 'www.theaims.ac.in') {
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '390896280167125');
+            fbq('track', 'PageView');
+          }
+          `}
+        </Script>
+        {/* End Facebook Pixel */}
+
+        {/* Facebook Pixel (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=390896280167125&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Facebook Pixel (noscript) */}
+
         {/* Defer any external CSS from staging.theaims.ac.in to avoid render-blocking */}
         <Script id="defer-external-styles" strategy="afterInteractive">
           {`
